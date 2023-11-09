ba07ea7522cba43cf7ec80b080fddad2b236cfee
Maria Ivanova <mariaivanova@Marias-MacBook-Pro.local>
2023-11-09 13:31:16 +0200

    Create app_test.py

---
+++ app_test.py
@@ -0,0 +1,7 @@
+import unittest
+from src.app import add
+
+class TestApp(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(2, 3), 5)
+        self.assertEqual(add(-1, 1), 0)
