ec4dd81b9ff2a8bc961b3efed65e1251cc9d9cea
Maria Ivanova <mariaivanova@Marias-MacBook-Pro.local>
2023-11-09 13:47:04 +0200

    Update app_test.py

--- src/app_test.py
+++ src/app_test.py
@@ -9,7 +9,7 @@ class TestApp(unittest.TestCase):
     def test_hello_world(self):
         response = self.app.get('/')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data.decode('utf-8'), 'Hello! I am a Flask application running on YOUR_HOSTNAME_HERE')
+        self.assertEqual(response.data.decode('utf-8'), 'Hello! I am a Flask application running on Marias-MacBook-Pro.local')
 
 if __name__ == '__main__':
     unittest.main()
