6dd5101cb3a46a9c81e854aab164555da5bfb21c
Maria Ivanova <mariaivanova@Marias-MacBook-Pro.local>
2023-11-10 12:05:11 +0200

    Updated python files style

--- tests/app_test.py
+++ tests/app_test.py
@@ -10,7 +10,8 @@ class TestApp(unittest.TestCase):
     def test_hello_world(self):
         response = self.app.get('/')
         self.assertEqual(response.status_code, 200)
-        expected_message = f'Hello! I am a Flask application running on {socket.gethostname()}'
+        expected_message = f'Hello! I am a Flask application running on /
+		{socket.gethostname()}'
         self.assertEqual(response.data.decode('utf-8'), expected_message)
 
 
