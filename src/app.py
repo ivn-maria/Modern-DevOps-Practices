1d9d943eb2659f83c3f473b6d173a24563db31fe
Maria I <96080313+ivn-maria@users.noreply.github.com>
2023-11-16 10:58:37 +0200

    Update app.py

--- src/app.py
+++ src/app.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', methods=['POST'])
+@app.route('/')
 def hello_world():
     return (
         "Hello! I am a Flask application running on {}"
