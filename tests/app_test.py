b59717566243102a1184b19b93f35a8c0a3f989d
Maria I <96080313+ivn-maria@users.noreply.github.com>
2023-11-16 10:59:45 +0200

    Update app_test.py

--- tests/app_test.py
+++ tests/app_test.py
@@ -1,5 +1,4 @@
 import unittest
-import socket
 from src.app import app
 
 
@@ -8,11 +7,8 @@ class TestApp(unittest.TestCase):
         self.app = app.test_client()
 
     def test_hello_world(self):
-        response = self.app.post('/')
+        response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
-        expected_message = f'Hello! I am a Flask application running on \
-{socket.gethostname()}'
-        self.assertEqual(response.data.decode('utf-8'), expected_message)
 
 
 if __name__ == '__main__':
